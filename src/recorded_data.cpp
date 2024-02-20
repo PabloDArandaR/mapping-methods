914795a63b59f57e9bde94088271d80f8c958ad5
Pablo <pablo.arandarod@gmail.com>
2024-02-20 21:46:23 +0100

    Clang formatting.

--- src/recorded_data.cpp
+++ src/recorded_data.cpp
@@ -1,8 +1,8 @@
-#include <iostream>
+#include <Eigen/Dense>
 #include <fstream>
+#include <iostream>
 #include <sstream>
 #include <vector>
-#include <Eigen/Dense>
 
 #include "mapping/mapping.hpp"
 #include "mapping/no_noise.hpp"
@@ -15,8 +15,8 @@ int main() {
 
     // Set the scaner metadata
     float angle_max{1}, angle_min{-1};
-    float n_measures {150};
-    float angle_increment {(angle_max - angle_min)/n_measures};
+    float n_measures{150};
+    float angle_increment{(angle_max - angle_min) / n_measures};
     scaner.set_angledata(angle_increment, angle_min, angle_max);
     scaner.set_rangedata(0.3, 15);
 
@@ -34,17 +34,17 @@ int main() {
     // Setup recorded data
     std::string data_line;
     std::ifstream data_file("../recording_4.csv");
-    while(std::getline(data_file, data_line)){
+    while (std::getline(data_file, data_line)) {
         std::istringstream iss(data_line);
         char comma;
         float posX, posY, posZ, q0, qX, qY, qZ;
         iss >> posX >> comma >> std::ws >> posY >> comma >> std::ws >> posZ >> comma >> std::ws;
-        iss >> q0 >> comma >> std::ws >> qX >> comma >> std::ws >> qY >> comma >> std::ws >> qZ/*  >> comma >> std::ws */;
+        iss >> q0 >> comma >> std::ws >> qX >> comma >> std::ws >> qY >> comma >> std::ws >> qZ /*  >> comma >> std::ws */;
 
         std::vector<float> measurements(150, 0.0);
         float value{0};
         int i{0};
-        while (iss >> std::ws >> comma >> value){
+        while (iss >> std::ws >> comma >> value) {
             measurements[i] = value;
             ++i;
         }
